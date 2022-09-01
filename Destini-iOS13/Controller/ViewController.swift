a2d8b5f621d82d68956716c1af3c7a5fb675885e
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-01 01:20:40 -0500

    Creación de modelos y decisiones para los cambios dinámicos

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain: StoryBrain = StoryBrain()
+    var currentStory: Story {
+        storyBrain.getCurrentStory()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.layer == choice1Button.layer {
+            storyBrain.setNextStory(destination: currentStory.choice1Destination)
+        } else {
+            storyBrain.setNextStory(destination: currentStory.choice2Destination)
+            
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = currentStory.title
+        choice1Button.setTitle(currentStory.choice1, for: .normal)
+        choice2Button.setTitle(currentStory.choice2, for: .normal)
+    }
+    
 }
 
